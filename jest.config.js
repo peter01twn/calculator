c98b4d5a449e83d48b7b48196bac99289f793874
peter <peter01twn@gmail.com>
2021-01-09 19:40:59 +0800

    use prettier to format

--- jest.config.js
+++ jest.config.js
@@ -2,14 +2,14 @@ const { pathsToModuleNameMapper } = require('ts-jest/utils');
 const { compilerOptions } = require('./tsconfig');
 
 module.exports = {
-  preset: 'jest-preset-angular',
-  roots: ['<rootDir>/src/'],
-  testMatch: ['**/+(*.)+(spec).+(ts)'],
-  setupFilesAfterEnv: ['<rootDir>/src/test.ts'],
-  collectCoverage: true,
-  coverageReporters: ['html'],
-  coverageDirectory: 'coverage/my-app',
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths || {}, {
-    prefix: '<rootDir>/'
-  })
+    preset: 'jest-preset-angular',
+    roots: ['<rootDir>/src/'],
+    testMatch: ['**/+(*.)+(spec).+(ts)'],
+    setupFilesAfterEnv: ['<rootDir>/src/test.ts'],
+    collectCoverage: true,
+    coverageReporters: ['html'],
+    coverageDirectory: 'coverage/my-app',
+    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths || {}, {
+        prefix: '<rootDir>/',
+    }),
 };
