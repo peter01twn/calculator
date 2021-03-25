6760368fef891f12b53eab59c01a3d4839d94711
peter <peter01twn@gmail.com>
2021-03-25 20:48:23 +0800

    fix Collect checkbox value accessor bug

--- src/app/modules/collect-checkbox/collect-checkbox-control-accessor.directive.ts
+++ src/app/modules/collect-checkbox/collect-checkbox-control-accessor.directive.ts
@@ -1,5 +1,5 @@
 import { Directive, ElementRef, forwardRef, HostListener, Input, Renderer2 } from '@angular/core';
-import { CheckboxControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { CollectCheckboxValue } from './collect-checkbox-form-control';
 
 const CUST_CHECKBOX_VALUE_ACCESSOR: any = {
@@ -16,8 +16,38 @@ const CUST_CHECKBOX_VALUE_ACCESSOR: any = {
     selector: 'input[type=checkbox][appCollectCheckbox]',
     providers: [CUST_CHECKBOX_VALUE_ACCESSOR],
 })
-export class CollectCheckboxControlAccessorDirective extends CheckboxControlValueAccessor {
+export class CollectCheckboxControlAccessorDirective implements ControlValueAccessor {
     @Input() value: any;
+
+    onChange: (value) => {};
+    onTouched: () => {};
+
+    /**
+     * Creates an instance of cust checkbox control accessor directive.
+     * @param renderer renderer
+     * @param elementRef elementRef
+     */
+    constructor(private renderer: Renderer2, private elementRef: ElementRef) {}
+
+    /**
+     * Writes value
+     * @param valueAry value
+     */
+    writeValue(valueAry: any[]): void {
+        this.renderer.setProperty(this.elementRef.nativeElement, 'checked', valueAry.includes(this.value));
+    }
+
+    setDisabledState(isDisabled: boolean): void {
+        this.renderer.setProperty(this.elementRef.nativeElement, 'disabled', isDisabled);
+    }
+
+    registerOnChange(fn: any): void {
+        this.onChange = fn;
+    }
+
+    registerOnTouched(fn: any): void {
+        this.onTouched = fn;
+    }
 
     /**
      * Hosts listener
@@ -26,22 +56,5 @@ export class CollectCheckboxControlAccessorDirective extends CheckboxControlValu
     @HostListener('change', ['$event.target.checked'])
     viewChange(checked: boolean): void {
         this.onChange(new CollectCheckboxValue(this.value, checked));
-    }
-
-    /**
-     * Creates an instance of cust checkbox control accessor directive.
-     * @param renderer renderer
-     * @param elementRef elementRef
-     */
-    constructor(private renderer: Renderer2, private elementRef: ElementRef) {
-        super(renderer, elementRef);
-    }
-
-    /**
-     * Writes value
-     * @param valueAry value
-     */
-    writeValue(valueAry: any[]): void {
-        this.renderer.setProperty(this.elementRef.nativeElement, 'checked', valueAry.includes(this.value));
     }
 }
